597400c898a427172a0c9ec787563f0dc5d20a0e
alex sharamok <alexanders@uniform.dev>
2024-12-03 14:33:24 +0200

    add initial search (not used)

--- src/pages/[[...slug]].tsx
+++ src/pages/[[...slug]].tsx
@@ -1,9 +1,28 @@
-import { CANVAS_DRAFT_STATE, CANVAS_PUBLISHED_STATE } from '@uniformdev/canvas';
+import { CANVAS_DRAFT_STATE, CANVAS_PUBLISHED_STATE, EntryData, ContentClient, flattenValues } from '@uniformdev/canvas';
 import { withUniformGetStaticProps, prependLocale } from '@uniformdev/canvas-next/route';
-import { getBreadcrumbs, getProjectMapClient, getRouteClient } from '../utilities/canvas/canvasClients';
+import { getBreadcrumbs, getProjectMapClient, getRouteClient, getContentClient } from '../utilities/canvas/canvasClients';
 export { default } from '../components/BasePage';
 
+// @ts-ignore: It is assumed that each application implements the modules at the appropriate location
+import { getFilterValues, mapUniformContentEntryFields } from '@/modules/search/utils';
+// @ts-ignore: It is assumed that each application implements the modules at the appropriate location
+import { FilterBy } from '@/modules/search/EntryFilterBox';
+// @ts-ignore: It is assumed that each application implements the modules at the appropriate location
+import { ARRAY_OPERATORS } from '@/modules/search/UniformEntrySearchProvider';
+// @ts-ignore: It is assumed that each application implements the localeSettings json at the appropriate location
+import localizationSettings from '@/context/locales.json';
+// @ts-ignore: It is assumed that each application implements the utilities at the appropriate location
+import { findSlotsWithType } from '@/utilities';
+
 // Doc: https://docs.uniform.app/docs/guides/composition/url-management/routing/slug-based-routing
+
+const getMemoizedContentClient = (() => {
+  let canvasClient: ContentClient;
+  return () => {
+    if (!canvasClient) canvasClient = getContentClient();
+    return canvasClient;
+  };
+})();
 
 export const getStaticProps = withUniformGetStaticProps({
   requestOptions: context => ({
@@ -29,8 +48,60 @@ export const getStaticProps = withUniformGetStaticProps({
       urlSegments: typeof slug === 'string' ? slug?.split('/') : slug,
     });
 
+    const uniformEntrySearchEngine = findSlotsWithType(composition, 'uniformEntrySearchEngine');
+
+    const _locale = _context.locale || _context.defaultLocale || 'en-US';
+
+    //When we have a search engine component in the composition, we need to fetch initial the search results
+    let initialSearchResults = {};
+    if (uniformEntrySearchEngine) {
+      const entryFilterBox = findSlotsWithType(composition, 'entryFilterBox');
+
+      const initialFiltersValue: EntryData[] = uniformEntrySearchEngine.parameters?.initialFilters
+        ?.value as EntryData[];
+
+      const filterByValue = entryFilterBox?.parameters?.filterBy?.value as EntryData[];
+
+      const filterBy = filterByValue?.map((filter: EntryData) => flattenValues(filter)) || [];
+
+      const initialFilters = initialFiltersValue
+        .map((filter: EntryData) => flattenValues(filter) as { key: string; operator: string; value: string })
+        .reduce<Record<string, unknown>>((acc, { key, operator, value }) => {
+          acc[key] = ARRAY_OPERATORS.includes(operator) ? { [operator]: value.split('|') } : { [operator]: value };
+          return acc;
+        }, {});
+
+      const response = await getMemoizedContentClient().getEntries({
+        filters: { ...initialFilters },
+        withTotalCount: true,
+        locale: _locale,
+      });
+
+      const { entries = [], totalCount = 0 } = response;
+      const resultEntries = entries.map(mapUniformContentEntryFields);
+
+      const avaliableFilters = getFilterValues(resultEntries, filterBy as FilterBy[]);
+
+      initialSearchResults = {
+        avaliableFilters,
+        resultEntries,
+        totalCount,
+        filterBy,
+      };
+    }
+
+    const translations = await import(`@/locales/${_context.locale || 'en-US'}.json`)
+      .then(m => m.default)
+      .catch(() => ({}));
+
     return {
-      props: { preview, data: composition || null, context: { breadcrumbs } },
+      props: { 
+        preview, 
+        data: composition || null,
+        context: { breadcrumbs, initialSearchResults },
+        localizationSettings,
+        translations,
+      },
     };
   },
 });
